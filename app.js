c540dc56ff170a5b4d9a728227d8a755940c252f
Ahhhshen <113367649+Ahhhshen@users.noreply.github.com>
2022-10-11 21:36:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app=express();
 
 app.use(express.json());
 
+const fs = require('fs')
 
 // Read and parse local josn file every time a new IP clicked in
-function readLocalFile(req,res,next){
-    express.readFile('./package.json','utf-8',(err,fileData)=>{
+function readLocalFile(filePath,cb){
+    fs.readFile(filePath,'utf-8',(err,fileData)=>{
         if(err){
             return cb && cb(err);
         }
@@ -17,39 +18,41 @@ function readLocalFile(req,res,next){
             return cb && cb(err);
         }
     });
-    next();
 }
+//Write newdata to json file
+const data = {
+    name: '000',     //take in the current IP
+    counter:1,       // incremented counter from this IP
+    face_points:[0], //arry of the collected points
+};
 
-app.get("/",(req,res)=>{
+readLocalFile('./01.json',(err,data)=>{
+    if(err){
+        console.log(err);
+    } else {
+      data.counter += 1;
+      fs.writeFile('./01.json',JSON.stringify(data,null,2),err => {
+            if(err){
+                console.log(err);
+            }else{
+                console.log('File successfully written!');
+            }
+        });
+    }
+});
+//———————————————————————————————————————————————————————————
+app.use(express.json());
+
+app.get("./package.json",(req,res)=>{
     //change to post the json file we read from localfile
     res.status(200).json({
         message:"get message",
     });
 });
 
-//Write newdata to json file
-const newData = {
-    name: '', //take in the current IP
-    counter:'' ,// incremented counter from this IP
-    face_points:'' ,//arry of the collected points
-};
 
-app.save("/newface",(req,res)=>{
-    readLocalFile(req,res,next);{
-        if(err){
-            console.log(err);
-        }
-        else {
-            // modify newData here
-            express.writeFile('./package,json',JSON.stringify(newData,null,2),err => {
-                if(err){
-                    console.log(err);
-                }else{
-                    console.log('File successfully written!');
-                }
-            });
-        }
-    };
+app.post("/newface",(req,res)=>{
+
     
     console.log(req.body);
     res.status(200).json({
