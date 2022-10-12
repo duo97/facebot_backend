94dae1653a8a54313e06601d88a25e762e7ba7f1
Ahhhshen <113367649+Ahhhshen@users.noreply.github.com>
2022-10-11 20:13:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ const app=express();
 app.use(express.json());
 
 
-//fill in: read and parse local json file
+// Read and parse local josn file every time a new IP clicked in
 function readLocalFile(req,res,next){
-    console.log("reading from localfile");
+    express.readFile('./package.json','utf-8',(err,fileData)=>{
+        if(err){
+            return cb && cb(err);
+        }
+        try{
+            const object = JSON.parse(fileData);
+            return cb && cb(null,object);
+        }catch(err){
+            return cb && cb(err);
+        }
+    });
     next();
 }
 
@@ -17,12 +27,34 @@ app.get("/",(req,res)=>{
     });
 });
 
-app.post("/newface",(req,res)=>{
-    //cahnge to save the json we get from the request to json file
+//Write newdata to json file
+const newData = {
+    name: '', //take in the current IP
+    counter:'' ,// incremented counter from this IP
+    face_points:'' ,//arry of the collected points
+};
+
+app.save("/newface",(req,res)=>{
+    readLocalFile(req,res,next);{
+        if(err){
+            console.log(err);
+        }
+        else {
+            // modify newData here
+            express.writeFile('./package,json',JSON.stringify(newData,null,2),err => {
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log('File successfully written!');
+                }
+            });
+        }
+    };
+    
     console.log(req.body);
     res.status(200).json({
         message:"post message",
     });
 });
 
-app.listen(4000,()=>console.log("server running on port 4000"));
+app.listen(4000,()=>console.log("server listening on port 4000"));
